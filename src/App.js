22e1620fbef47bdd359a72c4e0429e0a7318f216
nasim1903 <nasimahmed0113@gmail.com>
2022-09-25 23:47:14 +0100

    Created card carasouel

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import CSS from './components/Cards/images/CSSlogo.png'
 import JS from './components/Cards/images/JSlogo.png'
 import HTML from './components/Cards/images/HTMLsign.png'
+import Reactlogo from './components/Cards/images/Reactlogo.png'
 
 
 function App() {
@@ -16,7 +17,10 @@ function App() {
       <h2>SKILLS</h2>
       <div className='skills' >
         <Cards images={JS} alt={'JS icon'} title={'Javascript'} text={'Proficient experience in'}></Cards>
+        <Cards images={Reactlogo} alt={'React icon'} title={'React'} text={'Professional experience in react'}></Cards>
         <Cards images={CSS} alt={'css icon'} title={'CSS3'} text={'Expert Knowledge in'}></Cards>
+        <Cards images={HTML} alt={'HTML icon'} title={'HTML5'} text={'Knowledge in'}></Cards>
+        <Cards images={HTML} alt={'HTML icon'} title={'HTML5'} text={'Knowledge in'}></Cards>
         <Cards images={HTML} alt={'HTML icon'} title={'HTML5'} text={'Knowledge in'}></Cards>
       </div>
 
