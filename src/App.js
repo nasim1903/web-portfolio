288933d467f44883d6d6ce418f2e8283f4a57466
nasim1903 <nasimahmed0113@gmail.com>
2022-09-26 15:52:42 +0100

    Finished up on adding skills

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Reactlogo from './components/Cards/images/Reactlogo.png'
 import nodeIcon from './components/Cards/images/node.png'
 import mongo from './components/Cards/images/mongo.png'
 import expressIcon from './components/Cards/images/expressjs.png' 
+import sqlIcon from './components/Cards/images/postgres.png'
+import gitIcon from './components/Cards/images/git.png'
 
 
 function App() {
@@ -22,11 +24,12 @@ function App() {
         <div className='start' >
         <Cards images={JS} alt={'JS icon'} title={'Javascript'} text={'Proficient experience in'}></Cards>
         </div>
-        <Cards images={Reactlogo} alt={'React icon'} title={'React'} text={'Creating Wrb Apps in'}></Cards>
+        <Cards images={Reactlogo} alt={'React icon'} title={'React'} text={'Creating Web Apps in'}></Cards>
         <Cards images={nodeIcon} alt={'Node JS icon'} title={'NodeJS'} text={'Practical experience creating Back Ends using'}></Cards>
-        <Cards images={mongo} alt={'MongoDB icon'} title={'MongoDB'} text={'Experienced in creating NoSQL databases using'}></Cards>
         <Cards images={expressIcon} alt={'Express JS icon'} title={'ExpressJS'} text={'Created servers and REST APIs for Back Ends using'}></Cards>
-        <Cards images={HTML} alt={'HTML icon'} title={'HTML5'} text={'Knowledge in'}></Cards>
+        <Cards images={mongo} alt={'MongoDB icon'} title={'MongoDB'} text={'Experienced in creating NoSQL databases using'}></Cards>
+        <Cards images={sqlIcon} alt={'Postgres icon'} title={'PostgresSQL'} text={'On top of NoSQL databases i have used SQL databases such as'}></Cards>
+        <Cards images={gitIcon} alt={'Git Icon'} title={'Git'} text={'Mangaged projects such as this portfolio site using'}></Cards>
         <Cards images={HTML} alt={'HTML icon'} title={'HTML5'} text={'Knowledge in'}></Cards>
         <Cards images={CSS} alt={'css icon'} title={'CSS3'} text={'Expert Knowledge in'}></Cards>
       </div>
